a8842e0583e8283a207b42c61e73c85251bf2276
DeepthiEttamsethi <138504290+DeepthiEttamsethi@users.noreply.github.com>
2023-09-16 21:17:19 +0530

    Add files via upload

---
+++ Hibernate/java/TestCase1.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCase1 {
+	
+	@Test
+	
+	public void Testmethod()
+		{
+			
+			Test1 obj = new Test1();
+		
+			assertEquals(obj.Msg(),"Welcome to Maven Project");
+		
+	}
+}
